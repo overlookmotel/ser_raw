01978170c8a61d8a74764e6a5f9f4164b9557fc5
overlookmotel <theoverlookmotel@gmail.com>
2023-03-02 20:17:27 +0000

    Reorder consts [refactor]

--- ser_raw/src/aligned_serializer.rs
+++ ser_raw/src/aligned_serializer.rs
@@ -28,10 +28,25 @@ pub struct AlignedSerializer<const OUTPUT_ALIGNMENT: usize, const VALUE_ALIGNMEN
 impl<const OUTPUT_ALIGNMENT: usize, const VALUE_ALIGNMENT: usize>
 	AlignedSerializer<OUTPUT_ALIGNMENT, VALUE_ALIGNMENT>
 {
-	/// Assertions for validity of alignment consts.
+	/// Alignment of output buffer
+	pub const OUTPUT_ALIGNMENT: usize = OUTPUT_ALIGNMENT;
+
+	/// Typical alignment of values being serialized
+	pub const VALUE_ALIGNMENT: usize = VALUE_ALIGNMENT;
+
+	/// Maximum capacity of output buffer.
+	/// Dictated by the requirements of
+	/// [`alloc::Layout`](https://doc.rust-lang.org/alloc/alloc/struct.Layout.html).
+	/// "`size`, when rounded up to the nearest multiple of `align`, must not
+	/// overflow `isize` (i.e. the rounded value must be less than or equal to
+	/// `isize::MAX`)".
+	pub const MAX_CAPACITY: usize = isize::MAX as usize - (Self::OUTPUT_ALIGNMENT - 1);
+
+	/// Assertions for validity of alignment const params.
 	/// These assertions are not evaluated here.
-	/// `ASSERT_ALIGNMENTS_VALID` must be referenced in all code paths creating an
-	/// `AlignedSerializer`, to ensure compile-time error if assertions fail.
+	/// `Self::ASSERT_ALIGNMENTS_VALID` must be referenced in all code paths
+	/// creating an `AlignedSerializer`, to ensure compile-time error if
+	/// assertions fail.
 	const ASSERT_ALIGNMENTS_VALID: () = {
 		assert!(OUTPUT_ALIGNMENT > 0, "OUTPUT_ALIGNMENT must be 1 or more");
 		assert!(
@@ -52,17 +67,6 @@ impl<const OUTPUT_ALIGNMENT: usize, const VALUE_ALIGNMENT: usize>
 			"VALUE_ALIGNMENT must be a power of 2"
 		);
 	};
-	/// Maximum capacity of output buffer.
-	/// Dictated by the requirements of
-	/// [`alloc::Layout`](https://doc.rust-lang.org/alloc/alloc/struct.Layout.html).
-	/// "`size`, when rounded up to the nearest multiple of `align`, must not
-	/// overflow `isize` (i.e. the rounded value must be less than or equal to
-	/// `isize::MAX`)".
-	pub const MAX_CAPACITY: usize = isize::MAX as usize - (Self::OUTPUT_ALIGNMENT - 1);
-	/// Alignment of output buffer
-	pub const OUTPUT_ALIGNMENT: usize = OUTPUT_ALIGNMENT;
-	/// Typical alignment of values being serialized
-	pub const VALUE_ALIGNMENT: usize = VALUE_ALIGNMENT;
 
 	/// Create new Serializer with minimal memory pre-allocated.
 	/// without allocating any memory for output buffer.
