f3e4e734c77d94959fb8b76faad7a02f17871ab4
overlookmotel <theoverlookmotel@gmail.com>
2023-03-12 12:03:22 +0000

    Code comments [nocode]

--- ser_raw/src/serialize.rs
+++ ser_raw/src/serialize.rs
@@ -1,13 +1,64 @@
 use crate::Serializer;
 
 /// Trait for types which can be serialized.
+///
+/// Usually implemented with the derive macro `#[derive(Serializer)]`.
+///
+/// # Example
+///
+/// ```
+/// use ser_raw::{Serialize, Serializer};
+///
+/// struct Foo {
+///   smalls: Vec<u8>,
+///   bigs: Vec<u32>,
+/// }
+///
+/// impl<S> Serialize<S> for Foo
+/// where S: Serializer
+/// {
+/// 	fn serialize_data(&self, serializer: &mut S) {
+/// 		self.smalls.serialize_data(serializer);
+/// 		self.bigs.serialize_data(serializer);
+/// 	}
+/// }
+/// ```
 pub trait Serialize<Ser: Serializer> {
 	#[allow(unused_variables)]
 	#[inline(always)]
 	fn serialize_data(&self, serializer: &mut Ser) {}
 }
 
-/// Trait for use with `#[ser_with]`.
+/// Trait for implementing an equivalent of `Serialize` on foreign types for
+/// which it's not possible to implement `Serialize` directly due to orphan
+/// rules. Use with `#[ser_with]`.
+///
+/// # Example
+///
+/// ```
+/// use ser_raw::{Serializer, SerializeWith};
+///
+/// // The foreign type we want to be able to serialize
+/// use num_bigint::BigUint;
+///
+/// // Our own type which contains the foreign type
+/// #[derive(Serialize)]
+/// struct Foo {
+/// 	#[ser_with(BigUintProxy)]
+/// 	big: BigInt,
+/// }
+///
+/// struct BigUintProxy;
+/// impl<S> SerializeWith<BigUint, S> for BigUintProxy
+/// where S: Serializer
+/// {
+/// 	fn serialize_data_with(biguint: &BigUint, serializer: &mut S) {
+/// 		let bytes = biguint.to_bytes_le();
+/// 		serializer.push(&bytes.len());
+/// 		serializer.push_bytes(bytes.as_slice());
+/// 	}
+/// }
+/// ```
 pub trait SerializeWith<T, Ser: Serializer> {
 	fn serialize_data_with(t: &T, serializer: &mut Ser) -> ();
 }
